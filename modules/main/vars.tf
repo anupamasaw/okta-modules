24b441e92286eabfa83dfd13ce82d6db744cdc9f
anupamasaw <56156879+anupamasaw@users.noreply.github.com>
2020-10-01 14:21:08 +0530

    Update vars.tf

--- modules/main/vars.tf
+++ modules/main/vars.tf
@@ -1,25 +1,25 @@
 
 ### User ###
 variable "okta_user_first_name"{
-  default = "Suhnbbanbba" 
+  default = "Suhnndnbbanbba" 
 }
 variable "okta_user_last_name"{
-  default = "Sinbghnnha"
+  default = "Sindndbghnnha"
 }
 variable "okta_user_login"{
-  default = "sohbannbnana@gmail.com"
+  default = "sohbddannbnana@gmail.com"
 }
 variable "okta_user_email"{
-  default = "sohgjhanabjba@gmail.com"
+  default = "sohgddjhanabjba@gmail.com"
 }
 
 ### Group ###
 
 variable "okta_group_name"{
-  default = "prjbodubjnction envaabjb"
+  default = "prjbddodubjnction envaabjb"
 }
 variable "okta_group_description"{
-  default = "this is pbjnfbronmmd group envaanb"
+  default = "thidds is pbjnfbronmmd group envaanb"
 }
 
 ### Auth Server ###
@@ -29,11 +29,11 @@ variable "okta_auth_server_audiences" {
 }
 
 variable "okta_auth_server_description" {
-  default = "My Examnbn  ple Anbuth Serverggaaar"
+  default = "My Examddnbn  ple Anbuth Serverggaaar"
 }
  
 variable "okta_auth_server_name"{
-  default = "debbnbmo aunknth serverhbhaaabjbk"
+  default = "debbnddbmo aunknth serverhbhaaabjbk"
 }
  
 variable "okta_auth_server_issuer_mode" {
@@ -44,23 +44,23 @@ variable "okta_auth_server_issuer_mode" {
 ### App Saml ###
 
 variable "okta_app_saml_label"{
-  default = "ufbmdhbnbhh"
+  default = "ufbmdhbnddbhh"
 }
 
 variable "okta_app_saml_sso_url" {
-  default = "http://demo.com"
+  default = "http://usern.com"
 }
 
 variable "okta_app_saml_recipient" {
-  default = "http://demo.com"
+  default = "http://usern.com"
 }
 
 variable "okta_app_saml_destination" {
-  default = "http://demo.com"
+  default = "http://usern.com"
 }
 
 variable "okta_app_saml_audience" {
-  default = "http://demo.com/audience"
+  default = "http://usern.com/audience"
 }
 
 variable "okta_app_saml_subject_name_id_template" {
@@ -86,7 +86,7 @@ variable "okta_app_saml_honor_force_authn" {
 ### Inline Hook ###
 
 variable "okta_inline_hook_name"{
-  default = "demonbnnbnmnhgaag"
+  default = "demonbnnddbnmnhgaag"
 }
 
 variable "okta_inline_hook_version" {
@@ -109,7 +109,7 @@ variable "okta_idp_social_protocol_type" {
 }
 
 variable "okta_idp_social_name"{
-  default = "testhbbbnnbbjbjaaab"
+  default = "testhbbbdddnnbbjbjaaab"
 }
 
 
